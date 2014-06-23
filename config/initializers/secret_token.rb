bbf9b6369873d094f2c03c9f87d00c98edfc878f
dpoint01 <david.pointeau@tufts.edu>
2014-06-23 17:22:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fauteuil::Application.config.secret_key_base = 'afe491a6366d3fde13263dbdbf36d1281ef59b6fb8f931b76a1758e72e7e178f6589870f8c09f69927dc8d001fc9ba8e3faf100598df149c0ef9bdcfbbb1f23f'
