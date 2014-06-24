bdd2f50de9b8558022af8fc73125b8916bbcb06a
Patrick <pa@arthurfamily.ws>
2014-06-24 16:47:22 -0400

    added signout test

---
+++ spec/features/user_signout_spec.rb
@@ -0,0 +1,19 @@
+
+require 'rails_helper'
+
+feature 'guest sigout' do
+
+  scenario 'user signs out' do
+
+    login_as(FactoryGirl.create(:user), scope: :user)
+
+    visit chairs_path
+
+    click_on 'Sign out'
+
+
+    expect(page).to have_content('You need to sign in or sign up before continuing')
+
+  end
+
+end
