3e23ad59c7a7574fe8f167c25468a1679c416b2b
Rachael Serur <rachaelse@gmail.com>
2014-06-24 12:54:12 -0400

    Test created for adding review a for factory-made chair.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Fauteuil::Application.routes.draw do
+
   root 'chairs#index'
 
   resources :chairs, only: [:index, :show, :create, :new]
+
+  resources :chairs do
+    resources :reviews
+  end
 
   devise_for :users, :controllers => { :registrations => "registrations" }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Review, :type => :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should have_valid(:body)
+    .when('This is a review') }
+
+  it { should_not have_valid(:body)
+    .when('')}
+
+  it { should have_valid(:rating)
+    .when('1', '5', '1.5') }
+
+  it { should_not have_valid(:rating)
+    .when('-1', '5.5', '2.34') }
+
 end
