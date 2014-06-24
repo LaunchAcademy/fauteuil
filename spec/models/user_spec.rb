57648e3d387a05a925ed09f58a29974a2ea35597
Patrick <pa@arthurfamily.ws>
2014-06-24 16:22:16 -0400

    Added more tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,13 @@ it { should validate_uniqueness_of(:username) }
 
 it { should have_valid(:role).when('user','admin')}
 
+it { should have_valid(:email).when('user@gmail.com')}
+
+it { should_not have_valid(:email).when(nil,'')}
+
+it { should have_valid(:username).when('user123')}
+
+it { should_not have_valid(:username).when(nil,'')}
+
 
 end
