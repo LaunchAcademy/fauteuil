d6d1c7ceb5b87d58faa5be330d823d4bf163987c
Rachael Serur <rachaelse@gmail.com>
2014-06-25 14:32:00 -0400

    Fixed validations for half point increments.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,21 +1,21 @@
 class Review < ActiveRecord::Base
- belongs_to :chair
- belongs_to :user
- has_many :votes
+  belongs_to :chair
+  belongs_to :user
+  has_many :votes
 
- validate :body, presence: true
+  validate :body, presence: true
 
- #todo validating the range and the step increment
+  #todo validating the range and the step increment
 
-validate :rating, presence: true
-validate :rating, inclusion: { in: 1..5 }
-  # validate :rating_is_valid_increment
+  validates_numericality_of :rating, greater_than_or_equal_to: 0.0, less_than_or_equal_to: 5.0
 
-# def rating_is_valid_increment
-#   unless rating % 0.5 == 0
-#     errors.add(:rating, "must be in increments of 0.5 only.")
-#   end
-# end
+  validate :rating_is_valid_increment
+
+  def rating_is_valid_increment
+    if rating && rating % 0.5 != 0
+      errors.add(:rating, "must be in increments of 0.5 only.")
+    end
+  end
 
 end
 
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Review, :type => :model do
 
   it { should have_valid(:body).when('This is a review') }
 
-  it { should have_valid(:rating).when('1', '5') }
+  it { should have_valid(:rating).when(1, 5, 2.5) }
 
-  it { should_not have_valid(:rating).when('-1') }
+  it { should_not have_valid(:rating).when(-1, '', nil, 0.7) }
 
 end
