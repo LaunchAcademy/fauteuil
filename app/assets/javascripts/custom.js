2b56000f54d6c153b08ca5a49a493264825ea65d
Rachael Serur <rachaelse@gmail.com>
2014-06-25 19:58:26 -0400

    Further cleaned up pages.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,3 +2,25 @@
 function outputUpdate(val) {
     document.querySelector('#rating_show').value = val;
 }
+
+// Parallax effect for chair index
+$(document).ready(function() {
+
+    document.createElement("article");
+
+    $('div[data-type="background"]').each(function() {
+        var $bgobj = $(this);
+
+        $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+            var coords = '50%' + yPos + 'px';
+
+            $bgobj.css({
+                backgroundPosition: coords
+            });
+        });
+
+    });
+
+});
