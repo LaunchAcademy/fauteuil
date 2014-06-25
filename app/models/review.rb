92a911f5093499fc22b13e623854d5d93aff2f34
Rachael Serur <rachaelse@gmail.com>
2014-06-24 20:27:13 -0400

    Rating selection fixed.

--- app/models/review.rb
+++ app/models/review.rb
@@ -9,10 +9,12 @@ class Review < ActiveRecord::Base
   presence: true,
   inclusion: { in: 1..5 }
   validate :rating_is_valid_increment
-end
 
 def rating_is_valid_increment
   unless rating % 0.5 == 0
     errors.add(:rating, "must be in increments of 0.5 only.")
   end
 end
+
+end
+
