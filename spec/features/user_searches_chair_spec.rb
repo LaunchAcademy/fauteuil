50fd7d0c4ff58006ee64e7eb5db73b38309d66ac
dpoint01 <david.pointeau@tufts.edu>
2014-06-26 15:41:47 -0400

    Added search with layout

--- spec/features/user_searches_chair_spec.rb
+++ spec/features/user_searches_chair_spec.rb
@@ -2,9 +2,36 @@ require 'rails_helper'
 
 feature 'search for chair' do
 
+  let(:user) { FactoryGirl.create(:user) }
+  let(:chair) { FactoryGirl.create_list(:chair, 6)}
 
   scenario 'valid search by name' do
-    chairs = FactoryGirl.create_list(:chair, 6)
+    chair
 
+    login_as(user)
+    visit chairs_path
+
+    fill_in "search", with: "svaar"
+    click_button 'Search'
+
+    n = 0
+
+    6.times do
+       n += 1
+      expect(page).to have_content( 'svaar ' +  n.to_s )
+    end
+   end
+
+  scenario 'valid search by location' do
+    chair1 = FactoryGirl.create(:chair, location: 'Tokyo')
+
+    login_as(user)
+    visit chairs_path
+
+    fill_in "search", with: "Tok"
+    click_button 'Search'
+
+    expect(page).to have_content('Tokyo')
   end
+
 end
