7cda69a27a16791b2a3ef2160f4f5c5cebae0d18
Patrick <pa@arthurfamily.ws>
2014-06-25 21:51:54 -0400

    tests

--- spec/uploaders/picture_upload_new_spec.rb
+++ spec/uploaders/picture_upload_new_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
+
 feature 'user visits chairs new page to add a photo of a chair' do
 
   scenario 'User loads photo' do
 
-    user = FactoryGirl.create(:user)
-    login_as(user)
-
     visit '/chairs/new'
 
-    save_and_open_page
-    attach_file('Picture', 'spec/fixtures/avatar.jpeg')
+    # save_and_open_page
+    # attach_file('Picture', 'spec/fixtures/avatar.jpeg')
+
+
+   attach_file('chair_picture', 'spec/fixtures/avatar.jpeg')
+
+
 
     click_button 'Add Chair'
 
