95ac805f39b80434bd91cfb9653019b154bd6efd
Patrick <pa@arthurfamily.ws>
2014-06-26 11:40:14 -0400

    Tests Pass

--- spec/uploaders/picture_upload_new_spec.rb
+++ spec/uploaders/picture_upload_new_spec.rb
@@ -5,19 +5,28 @@ feature 'user visits chairs new page to add a photo of a chair' do
 
   scenario 'User loads photo' do
 
+    category = FactoryGirl.create(:category)
+    user = FactoryGirl.create(:user)
+    login_as(user)
     visit new_chair_path
 
-    # save_and_open_page
-    # attach_file('Picture', 'spec/fixtures/avatar.jpeg')
+    save_and_open_page
+
+    fill_in "Name", with: "Bean Bag"
+    fill_in "Location", with: "Boston"
+    fill_in "Manufacturer", with: "Ikea"
+    fill_in "Description", with: "This is a sleepy kind of chair"
+
+    select category.name, from: "chair[category_id]"
 
 
-   attach_file('chair_picture', 'spec/fixtures/avatar.jpeg')
+    attach_file('Picture', 'spec/fixtures/avatar.jpeg')
 
 
 
     click_button 'Add Chair'
 
-    expect(page).to have_content('Success!.')
+    expect(page).to have_content("Bean Bag")
 
   end
 end
--- spec/uploaders/picture_upload_show_spec.rb
+++ spec/uploaders/picture_upload_show_spec.rb
@@ -4,9 +4,19 @@ feature 'user visits chairs page to view a photo of a chair' do
 
   scenario "User visits chair page" do
 
+    chairs = FactoryGirl.create_list(:chair, 6)
+    user = FactoryGirl.create(:user)
+
+    login_as(user)
+
     visit chairs_path
 
-    page.should have_content("picture")
-
+    chairs.each do |chair|
+      expect(page).to have_content(chair.name, chair.picture)
+    end
   end
 end
+
+
+
+
