3fec71bbb3495ed2a591f57d4349315684a48f01
Patrick <pa@arthurfamily.ws>
2014-06-30 13:05:19 -0400

    Issue4

--- spec/uploaders/avatar_upload_spec.rb
+++ spec/uploaders/avatar_upload_spec.rb
@@ -1,25 +1,24 @@
-require 'rails_helper'
+require "rails_helper"
 
+feature "user visits registration uploads photo" do
 
-feature 'user visits registration uploads photo' do
-
-  scenario 'User loads photo' do
+  scenario "User loads photo" do
 
     user = FactoryGirl.create(:user)
 
     visit new_user_registration_path
 
     fill_in "Username", with: "rambo"
-    fill_in 'Email', with: 'bob@example.com'
-    fill_in 'Password', with: '12345678'
-    fill_in 'Password confirmation', with: '12345678'
-    attach_file('Avatar', 'spec/fixtures/avatar.jpeg')
+    fill_in "Email", with: "bob@example.com"
+    fill_in "Password", with: "12345678"
+    fill_in "Password confirmation", with: "12345678"
+    attach_file("Avatar", "spec/fixtures/avatar.jpeg")
 
-    click_button 'Sign up'
+    click_button "Sign up"
 
     save_and_open_page
 
-    expect(page).to have_content('You have signed up successfully.')
+    expect(page).to have_content("You have signed up successfully.")
 
   end
 end
