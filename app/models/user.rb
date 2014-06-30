484dbc2d979306d64fc99175005e9e55decdb456
Patrick <pa@arthurfamily.ws>
2014-06-30 13:30:21 -0400

    Issue6

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   mount_uploader :avatar, AvatarUploader
-  devise :database_authenticatable, :registerable,:recoverable,
-  :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :trackable, :validatable
   has_many :chairs
   has_many :reviews
   has_many :votes
