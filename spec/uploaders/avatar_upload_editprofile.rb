759af7a866dc3b31c517aec0f8f80dcc3a823860
Patrick <pa@arthurfamily.ws>
2014-07-01 10:37:47 -0400

    ImageFix

--- spec/uploaders/avatar_upload_editprofile.rb
+++ spec/uploaders/avatar_upload_editprofile.rb
@@ -2,16 +2,12 @@ require "rails_helper"
 
 feature "user visits edit profile to update information" do
 
-  scenario "User updates profile information" do
+  scenario "User changes photo" do
 
     user = FactoryGirl.create(:user)
     login_as(user)
     visit edit_user_registration_path
 
-    fill_in "Username", with: "rambo"
-    fill_in "Email", with: "bob@example.com"
-    fill_in "Password", with: "12345678"
-    fill_in "Password confirmation", with: "12345678"
     attach_file("Avatar", "spec/fixtures/avatar.jpeg")
     expect(page).to have_css("img[src='#{user.avatar_url}']")
 
