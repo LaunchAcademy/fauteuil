b9faf9154b2c3893c0096b8e16999f7a02999d1a
Patrick <pa@arthurfamily.ws>
2014-07-02 16:50:18 -0400

    Add

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,13 @@ require "rails_helper"
 describe User do
 
   it { should validate_uniqueness_of(:username) }
-  it { should have_valid(:role).when("user","admin")}
+  it { should have_valid(:role).when("user", "admin") }
 
-  it { should have_valid(:email).when("user@gmail.com")}
-  it { should_not have_valid(:email).when(nil,"")}
+  it { should have_valid(:email).when("user@gmail.com") }
+  it { should_not have_valid(:email).when(nil, "") }
 
-  it { should have_valid(:username).when("user123")}
-  it { should_not have_valid(:username).when(nil,"")}
+  it { should have_valid(:username).when("user123") }
+  it { should_not have_valid(:username).when(nil, "") }
 end
 
 describe "#admin?" do
