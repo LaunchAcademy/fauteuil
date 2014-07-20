88e962e6857809c5292cdd82984932a453c9b23a
Rachael Serur <rachaelse@gmail.com>
2014-07-19 20:09:31 -0400

    Corrected admin namespacing tests, SMTP settings.

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -6,12 +6,14 @@ if Rails.env.development?
   }
 end
 
-ActionMailer::Base.smtp_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
-    :domain =>         'heroku.com',
-    :authentication => :plain
-}
-ActionMailer::Base.delivery_method = :smtp
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+      :port =>           '587',
+      :address =>        'smtp.mandrillapp.com',
+      :user_name =>      ENV['MANDRILL_USERNAME'],
+      :password =>       ENV['MANDRILL_APIKEY'],
+      :domain =>         'heroku.com',
+      :authentication => :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
+end
