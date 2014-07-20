8bfc62860bdc87301922bc2830091efef325966f
Rachael Serur <rachaelse@gmail.com>
2014-07-20 12:36:02 -0400

    Addressed houndci issues.

--- spec/features/authorization_spec.rb
+++ spec/features/authorization_spec.rb
@@ -5,16 +5,12 @@ feature "user must be authorized to access certain pages", %q(
 ) do
 
   scenario "unregistered user cannot access admin page" do
-    assert(ActionController::RoutingError) {
-      visit admin_users_path
-    }
+    assert(ActionController::RoutingError) { visit admin_users_path }
   end
 
   scenario "unregistered user cannot access edit chair page" do
-   chair = FactoryGirl.create(:chair)
+    chair = FactoryGirl.create(:chair)
 
-   assert(ActionController::RoutingError) {
-    visit edit_chair_path(chair.id)
-  }
+    assert(ActionController::RoutingError) { visit edit_chair_path(chair.id) }
   end
 end
