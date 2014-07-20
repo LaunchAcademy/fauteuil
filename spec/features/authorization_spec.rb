fe5737dc1e63aa82ab60021574d911e4737c1829
Rachael Serur <rachaelse@gmail.com>
2014-07-19 21:09:28 -0400

    Added edit chair authorization test.

--- spec/features/authorization_spec.rb
+++ spec/features/authorization_spec.rb
@@ -4,10 +4,17 @@ feature "user must be authorized to access certain pages", %q(
 
 ) do
 
-
   scenario "unregistered user cannot access admin page" do
+    assert(ActionController::RoutingError) {
+      visit admin_users_path
+    }
+  end
+
+  scenario "unregistered user cannot access edit chair page" do
+   chair = FactoryGirl.create(:chair)
+
    assert(ActionController::RoutingError) {
-    visit admin_users_path
+    visit edit_chair_path(chair.id)
   }
   end
 end
